564186ca60af00774ae3e0ae44f9710e3593f2f2
Steven Pears <steven.pears@live.co.uk>
2023-12-31 16:23:34 +0000

    Add slug to organisation api

--- Robin.NetStandard/IOrganizationApi.cs
+++ Robin.NetStandard/IOrganizationApi.cs
@@ -6,6 +6,9 @@ namespace Robin.NetStandard;
 public interface IOrganizationApi
 {
     Task<ApiResponse<Organization?>?> Get(int id);
+    Task<ApiResponse<Organization?>?> Get(string slug);
     Task<PagedApiResponse<Location[]?>?> GetLocations(int orgId, string? query = null, int? page = null, int? perPage = null);
+    Task<PagedApiResponse<Location[]?>?> GetLocations(string slug, string? query = null, int? page = null, int? perPage = null);
     Task<PagedApiResponse<User[]?>?> GetUsers(int orgId, string? query = null, int? page = null, int? perPage = null, string[]? ids = null);
+    Task<PagedApiResponse<User[]?>?> GetUsers(string slug, string? query = null, int? page = null, int? perPage = null, string[]? ids = null);
 }
--- Robin.NetStandard/OrganizationApi.cs
+++ Robin.NetStandard/OrganizationApi.cs
@@ -1,4 +1,5 @@
 ﻿using Robin.NetStandard.Entities;
+using System.Security.Cryptography;
 
 namespace Robin.NetStandard;
 
@@ -13,19 +14,37 @@ public class OrganizationApi : IOrganizationApi
 
     public Task<ApiResponse<Organization?>?> Get(int id)
     {
-        return Client.MakeJsonCall<ApiResponse<Organization?>>(HttpMethod.Get, $"organizations/{id}");
+        return Get(id.ToString());
     }
 
-    public Task<PagedApiResponse<Location[]?>?> GetLocations(int orgId, string? query = null, int? page = null, int? perPage = null)
+    public Task<PagedApiResponse<Location[]?>?> GetLocations(int orgId, string? query = null, int? page = null,
+        int? perPage = null)
+    {
+        return GetLocations(orgId.ToString(), query, page, perPage);
+    }
+
+    public Task<PagedApiResponse<User[]?>?> GetUsers(int orgId, string? query = null, int? page = null,
+        int? perPage = null, string[]? ids = null)
+    {
+        return GetUsers(orgId.ToString(), query, page, perPage, ids);
+    }
+
+    public Task<ApiResponse<Organization?>?> Get(string slug)
+    {
+        return Client.MakeJsonCall<ApiResponse<Organization?>>(HttpMethod.Get, $"organizations/{slug}");
+    }
+
+    public Task<PagedApiResponse<Location[]?>?> GetLocations(string slug, string? query = null, int? page = null, int? perPage = null)
     {
         var prms = new Dictionary<string, string>();
         prms.AddIfNotEmpty(nameof(query), query);
         prms.AddIfNotEmpty(nameof(page), page?.ToString());
         prms.AddIfNotEmpty("per_page", perPage?.ToString());
-        return Client.MakeJsonCall<PagedApiResponse<Location[]?>>(HttpMethod.Get, $"organizations/{orgId}/locations", prms);
+        return Client.MakeJsonCall<PagedApiResponse<Location[]?>>(HttpMethod.Get, $"organizations/{slug}/locations", prms);
+
     }
 
-    public Task<PagedApiResponse<User[]?>?> GetUsers(int orgId, string? query = null, int? page = null, int? perPage = null, string[]? ids = null)
+    public Task<PagedApiResponse<User[]?>?> GetUsers(string slug, string? query = null, int? page = null, int? perPage = null, string[]? ids = null)
     {
         var prms = new Dictionary<string, string>();
         prms.AddIfNotEmpty(nameof(query), query);
@@ -36,6 +55,6 @@ public class OrganizationApi : IOrganizationApi
             prms.Add(nameof(ids), string.Join(",", ids));
         }
 
-        return Client.MakeJsonCall<PagedApiResponse<User[]?>>(HttpMethod.Get, $"organizations/{orgId}/users", prms);
+        return Client.MakeJsonCall<PagedApiResponse<User[]?>>(HttpMethod.Get, $"organizations/{slug}/users", prms);
     }
 }
